9d013aa68cc7da2a8d908ea711161d4a7ccd80c4
sayfullaev1999 <dima.sayfullaev@mail.ru>
2021-02-25 15:57:44 +0500

    Added Category model
    * Configure up the admin panel for the category model

--- library/admin.py
+++ library/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Category
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    fields = ('title', 'image', 'authors', 'price', 'amount')
+    fields = ('title', 'image', 'category', 'authors', 'price', 'amount')
     list_display = ('title',)
     list_filter = ('price', )
     search_fields = ('title', )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('name', )
+    search_fields = ('name', )
--- library/models.py
+++ library/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Book(models.Model):
     """
-    The Book model which is related to the User model ManyToMany.
+    The Book model which is related to the User model ManyToMany and related to the Book model OneToOne.
     All fields are required
     """
     title = models.CharField(_('title'), max_length=150, db_index=True)
@@ -13,6 +13,7 @@ class Book(models.Model):
     price = models.FloatField(_('price'))
     image = models.ImageField(_('image'), help_text='Cover book')
     amount = models.PositiveIntegerField(_('amount'))
+    category = models.OneToOneField('Category', on_delete=models.CASCADE, verbose_name='category')
 
 
     def __str__(self):
@@ -21,7 +22,23 @@ class Book(models.Model):
         """
         return self.title
 
-
     class Meta:
         verbose_name = _('book')
         verbose_name_plural = _('books')
+
+class Category(models.Model):
+    """
+    The Book model.
+    All fields are required
+    """
+    name = models.CharField(_('name'), max_length=50, db_index=True)
+
+    def __str__(self):
+        """
+        Return the category name
+        """
+        return self.name
+
+    class Meta:
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
