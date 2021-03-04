420e123008054fdccba605488a21e8b3ece90341
sayfullaev1999 <dima.sayfullaev@mail.ru>
2021-03-04 14:28:39 +0500

    Fixed errors

--- mysite/urls.py
+++ mysite/urls.py
@@ -10,7 +10,7 @@ from mysite.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
 
 if settings.DEBUG:
